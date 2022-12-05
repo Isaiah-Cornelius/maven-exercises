29a6689194e2863d3ac76d1ae217b5a843dff988
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-12-05 09:10:32 -0700

    Finished

--- src/main/java/HW.java
+++ src/main/java/HW.java
@@ -10,6 +10,14 @@ public class HW {
         System.out.print("Enter something: ");
         String userInput = sc.nextLine();
         System.out.println("You entered: " + userInput);
+        boolean isNum = isNumeric(userInput);
+        String isNumString;
+        if (isNum){
+            isNumString = " is a number";
+        } else {
+            isNumString = " is not a number";
+        }
+        System.out.println(userInput + isNumString);
         System.out.println("Flipped Case: " + swapCase(userInput));
         System.out.println("Reversed: " + reverse(userInput));
     }
